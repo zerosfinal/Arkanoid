51533f4c69a8710c142a4a67bc937a90e7dbd598
zerosfinal <zerosfinal@gmail.com>
2020-01-31 09:00:38 +0100

    1 commit

---
+++ app/src/main/java/com/example/arkanoid/MainMenu.java
@@ -0,0 +1,37 @@
+package com.example.arkanoid;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.arkanoid) {
+            Intent intent = new Intent(this, ArkanoidActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
